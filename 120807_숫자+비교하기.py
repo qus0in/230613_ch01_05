b07985d3d5cc476d4384489314375aeb1f8e10a4
new0in <newbie0in@gmail.com>
2023-06-13 12:46:31 +0900

    프로그래머스 숫자 비교하기

---
+++ 120807_숫자+비교하기.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=python3
+def solution(num1, num2):
+    # # num1, num2 -> 외부로부터 주어진(입력받은) 변수.
+    # answer = 0
+    # # 두수가 같으면 1
+    # if num1 == num2:
+    #     answer = 1
+    # # 다르면 -1
+    # # if num1 != num2:
+    # else:
+    #     answer = -1
+
+    # answer = ? if num1 == num2 else ?
+    # answer = 1 if num1 == num2 else -1
+
+    # return answer # 외부로 돌려준다 -> answer
+
+    return 1 if num1 == num2 else -1
